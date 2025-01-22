7d27ec45732dd2200aeabe19e681210a2fcc9880
Jacek Nowak <jaceknowakdev@gmail.com>
2025-01-22 19:40:26 +0100

    create ecs deployment pipeline

--- backend/src/main/java/dev/jlynx/magicaldrones/security/SecurityConfig.java
+++ backend/src/main/java/dev/jlynx/magicaldrones/security/SecurityConfig.java
@@ -6,7 +6,7 @@ import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
-import org.springframework.beans.factory.annotation.Autowired;
+import dev.jlynx.magicaldrones.exception.InternalServerException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -31,16 +31,20 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 import java.util.List;
 
 @Configuration
 @EnableMethodSecurity
 @EnableWebSecurity
 public class SecurityConfig {
-
-    @Autowired
-    private RsaKeyProperties rsaKeys;
-
 
     @Profile("dev")
     @Bean
@@ -53,6 +57,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers("/auth/token").permitAll()
                         .requestMatchers(HttpMethod.POST, "/account").permitAll()
+                        .requestMatchers("/actuator", "/actuator/**").permitAll()
                         .requestMatchers("/account/**", "/transform/**", "/image/**").hasRole("USER")
                         .requestMatchers("/dummy").authenticated()
                         .anyRequest().denyAll()
@@ -77,6 +82,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers("/auth/token").permitAll()
                         .requestMatchers(HttpMethod.POST, "/account").permitAll()
+                        .requestMatchers("/actuator", "/actuator/health", "/actuator/info").permitAll()
                         .requestMatchers("/account/**", "/transform/**", "/image/**").hasRole("USER")
                         .anyRequest().denyAll()
                 )
@@ -109,16 +115,52 @@ public class SecurityConfig {
 
     @Bean
     public JwtDecoder jwtDecoder() {
-        return NimbusJwtDecoder.withPublicKey(rsaKeys.getPublicKey()).build();
+        return NimbusJwtDecoder.withPublicKey(getPublicKey()).build();
     }
 
     @Bean
     public JwtEncoder jwtEncoder() {
-        JWK jwk = new RSAKey.Builder(rsaKeys.getPublicKey())
-                .privateKey(rsaKeys.getPrivateKey())
+        JWK jwk = new RSAKey.Builder(getPublicKey())
+                .privateKey(getPrivateKey())
                 .build();
         JWKSource<SecurityContext> source = new ImmutableJWKSet<>(new JWKSet(jwk));
         return new NimbusJwtEncoder(source);
+    }
+
+    public RSAPublicKey getPublicKey() {
+        String publicKeyPem = System.getenv("MD_RSA_PUBLIC_KEY");
+        if (publicKeyPem == null) {
+            throw new InternalServerException("MD_RSA_PUBLIC_KEY environment variable not set");
+        }
+        String publicKey = publicKeyPem.replace("-----BEGIN PUBLIC KEY-----", "")
+                .replace("-----END PUBLIC KEY-----", "")
+                .replaceAll("\\s", "");
+        byte[] decoded = Base64.getDecoder().decode(publicKey);
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(decoded);
+        try {
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            return (RSAPublicKey) keyFactory.generatePublic(keySpec);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new InternalServerException("Failed to parse private key", e);
+        }
+    }
+
+    public RSAPrivateKey getPrivateKey() {
+        String privateKeyPem = System.getenv("MD_RSA_PRIVATE_KEY");
+        if (privateKeyPem == null) {
+            throw new InternalServerException("MD_RSA_PRIVATE_KEY environment variable not set");
+        }
+        String privateKey = privateKeyPem.replace("-----BEGIN PRIVATE KEY-----", "")
+                .replace("-----END PRIVATE KEY-----", "")
+                .replaceAll("\\s", "");
+        byte[] decoded = Base64.getDecoder().decode(privateKey);
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(decoded);
+        try {
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            return (RSAPrivateKey) keyFactory.generatePrivate(keySpec);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new InternalServerException("Failed to parse private key", e);
+        }
     }
 
     @Bean
