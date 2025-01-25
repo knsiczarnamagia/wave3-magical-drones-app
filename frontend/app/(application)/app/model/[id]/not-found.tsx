77be21a72666b5ed63229547c169180b45b71cc3
Jacek Nowak <jaceknowakdev@gmail.com>
2025-01-25 19:40:27 +0100

    add conditional dashboard links in not-found pages

---
+++ frontend/app/(application)/app/model/[id]/not-found.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import PageTitle from '@/components/PageTitle';
+import styles from './not-found.module.scss';
+import { cookies } from 'next/headers';
+
+// todo: make this a client component?
+export default async function NotFound() {
+
+    const cookieStore = await cookies();
+    const isAuthorized = cookieStore.has('session');
+    
+    return (
+        <div className={styles.main}>
+            <PageTitle>Page not found!</PageTitle>
+            <p className={styles.text}>Could not find the requested resource.</p>
+            {isAuthorized && <Link href="/app" className={styles.link}>Back to app</Link>}
+            {!isAuthorized && <Link href="/" className={styles.link}>Back to home</Link>}
+        </div>
+    );
+}
--- frontend/app/not-found.tsx
+++ frontend/app/not-found.tsx
@@ -2,15 +2,22 @@ import Link from 'next/link';
 import PageTitle from '@/components/PageTitle';
 import styles from './not-found.module.scss';
 import { fontPrimary } from '@/lib/fonts';
+import { cookies } from 'next/headers';
 
-export default function NotFound() {
+// todo: make this a client component?
+export default async function NotFound() {
+
+    const cookieStore = await cookies();
+    const isAuthorized = cookieStore.has('session');
+
     return (
         <html lang="en" className={fontPrimary.className}>
             <body>
                 <main className={styles.main}>
                     <PageTitle>Page not found!</PageTitle>
                     <p className={styles.text}>Could not find the requested resource.</p>
-                    <Link href="/" className={styles.link}>Back to home</Link>
+                    {isAuthorized && <Link href="/app" className={styles.link}>Back to app</Link>}
+                    {!isAuthorized && <Link href="/" className={styles.link}>Back to home</Link>}
                 </main>
             </body>
         </html>
